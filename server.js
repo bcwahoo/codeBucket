8299e78572762c1e2543d3b9bc2b40831d73100b
Talib Shaikh <talibnisharshaikh@gmail.com>
2019-11-24 18:39:08 -0500

    Did API Routes

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: true, pool: false };
+// only run when testing server
+//var syncOptions = { force: true, pool: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
