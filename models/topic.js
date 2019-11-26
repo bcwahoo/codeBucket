615f88aa8a84f8e13e6ae94b5d2b3c282df82067
bcwahoo <bcwahoo@gmail.com>
2019-11-26 18:46:34 -0500

    lastchanges

--- models/topic.js
+++ models/topic.js
@@ -5,5 +5,12 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     }
   });
+  Topic.associate = function(models) {
+    // Associating Topic with Posts
+    // When an Topic is deleted, also delete any associated Posts
+    Topic.hasMany(models.Subject, {
+      onDelete: "cascade",
+    });
+  };
   return Topic;
 };
