fbee57bde8e294359205de081c6a40eb63652c53
bcwahoo <bcwahoo@gmail.com>
2019-12-03 18:48:38 -0500

    Adding Seed

--- seeders/topic-seed.js
+++ seeders/topic-seed.js
@@ -1,77 +1,82 @@
-"use strict";
+'use strict';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Topics", [
+    return queryInterface.bulkInsert('Topics', [
       {
-        topicName: "HTML",
+        topicName: 'HTML',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "CSS",
+        topicName: 'CSS',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "JavaScript",
+        topicName: 'JavaScript',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "JQuery",
+        topicName: 'JQuery',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Timers",
+        topicName: 'Timers',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Ajax",
+        topicName: 'Ajax',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Firebase",
+        topicName: 'Firebase',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Nodejs",
+        topicName: 'Nodejs',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "JS Constructors",
+        topicName: 'JS Constructors',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "MySQL",
+        topicName: 'MySQL',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Express",
+        topicName: 'Express',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Handelbars",
+        topicName: 'Handelbars',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        topicName: "Sequelize",
+        topicName: 'Sequelize',
         createdAt: new Date(),
-        updatedAt: new Date()
-      }
+        updatedAt: new Date(),
+      },
+      {
+        topicName: 'MongoDB',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("Topics", null, {});
-  }
+    return queryInterface.bulkDelete('Topics', null, {});
+  },
 };
