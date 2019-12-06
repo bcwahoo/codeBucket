9de17a908e7693e897b27afffc03f5db53bf824c
philldesign <53308938+philldesign@users.noreply.github.com>
2019-12-06 15:50:51 -0500

    Update card-seed.js
    HTML Cards updated

--- seeders/card-seed.js
+++ seeders/card-seed.js
@@ -37,7 +37,7 @@ module.exports = {
 
         createdAt: new Date(),
         updatedAt: new Date(),
-        SubjectId: 2
+        SubjectId: 1
       },
       {
         cardName: "HTML Elements",
@@ -57,9 +57,7 @@ module.exports = {
         cardDef:
           "An HTML form contains form elements. Form elements are different types of input elements, like text fields, checkboxes, radio buttons, submit buttons, and more.",
         example: `The <input> element is the most important form element.
-
           The <input> element can be displayed in several ways, depending on the type attribute. 
-          
           `,
 
         createdAt: new Date(),
@@ -77,6 +75,81 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
         SubjectId: 4
+      },
+        {
+        cardName: "HTML Table",
+        cardDef:
+          "An HTML table is defined with the <table> tag. Each table row is defined with the <tr> tag. A table header is defined with the <th> tag. By default, table headings are bold and centered. A table data/cell is defined with the <td> tag.",
+        example: `<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>`,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        SubjectId: 5
+      },
+      {
+        cardName: "HTML Attributes",
+        cardDef:
+          "HTML images also have width and height attributes, which specifies the width and height of the image:",
+        example: `<img src="img_girl.jpg" width="500" height="600">`,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        SubjectId: 6
+      },
+      {
+        cardName: "HTML Paragraphs",
+        cardDef:
+          "The HTML <p> element defines a paragraph:",
+        example: `<p>This is a paragraph.</p>
+                  <p>This is another paragraph.</p>`,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        SubjectId: 7
+      },
+      {
+        cardName: "HTML Unordered List",
+        cardDef:
+          "An unordered list starts with the <ul> tag. Each list item starts with the <li> tag. The list items will be marked with bullets (small black circles) by default.",
+        example: `<ul>
+                    <li>Coffee</li>
+                    <li>Tea</li>
+                    <li>Milk</li>
+                  </ul>`,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        SubjectId: 8
+      },
+      {
+        cardName: "HTML Ordered List",
+        cardDef:
+          "An unordered list starts with the <ol> tag. Each list item starts with the <li> tag. The list items will be marked with numbers by default.",
+        example: `<ol>
+                    <li>Coffee</li>
+                    <li>Tea</li>
+                    <li>Milk</li>
+                  </ol>`,
+
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        SubjectId: 9
       }
     ]);
   },
